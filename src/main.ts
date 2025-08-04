0fb163afeca44acda8946917f008d407cf86ebb7
Han Wong <han.wong@pm.me>
2025-08-04 09:20:16 +0200

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,12 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
 import io from "socket.io-client";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
-
 const socket = io(import.meta.env.VITE_SOCKET_URL);
-console.log(import.meta.env.VITE_SOCKET_URL)
 const form = document.getElementById("form") as HTMLFormElement;
 const input = document.getElementById("input") as HTMLInputElement;
 const messages = document.getElementById("messages") as HTMLUListElement;
 form?.addEventListener("submit", (e) => {
    e.preventDefault();
-   debugger
-   socket
    if (input?.value) {
       socket.emit("chat message", input.value);
       input.value = "";
@@ -37,7 +14,6 @@ form?.addEventListener("submit", (e) => {
 });
 
 socket.on("chat message", (msg) => {
-    debugger
    const item = document.createElement("li");
    item.textContent = msg;
    messages.appendChild(item);
